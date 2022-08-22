abfbc07800c5d53252621d0448dac7e59c63c161
PadalaRuchitha <98527784+PadalaRuchitha@users.noreply.github.com>
2022-08-22 23:25:51 +0530

    Assignment 3MVC

--- HomeController.cs
+++ HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Assignment_2.Controllers
+namespace Assignment_3_EF_.Controllers
 {
     public class HomeController : Controller
     {
